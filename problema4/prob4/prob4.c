2f32ef88c295ea4dc23522eec92f10b663d5e036
Darksw20 <ricardog.navi20@gmail.com>
2020-08-27 09:35:16 -0500

    Pruebas de codigo

---
+++ problema4/prob4/prob4.c
@@ -0,0 +1,39 @@
+#include "stdio.h"
+
+/*
+void strrem(char *input, char target){
+    while(*input != '\0'){
+        if(*input == target){
+            char *temp = input;
+            while(*input != '\0'){
+                *input = *(input + 1);
+                input++;
+            }
+            input = temp;
+        }else{
+            input++;
+        }
+    }
+}
+
+*/
+
+void strrem(char* input, char target) {
+    char* temp = input;
+    char* current = input;
+
+    while(*temp != '\0') {
+        if(*temp != target){ 
+            *current = *temp;
+            current++;
+        }
+        temp++;
+    }
+    *current = '\0';
+}
+
+int main(){
+    char hello[] = "Hola mundo";
+    strrem(hello,'o');
+    printf("%s\n",hello);
+}
