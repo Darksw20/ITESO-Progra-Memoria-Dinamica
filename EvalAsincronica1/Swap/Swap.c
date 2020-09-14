5b273ddb536dea424aa399b7596105f5fd14536f
MiliGuagu <57267175+MiliGuagu@users.noreply.github.com>
2020-09-13 22:01:41 -0500

    swap

--- EvalAsincronica1/Swap/Swap.c
+++ EvalAsincronica1/Swap/Swap.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
-/**
- * 
- */
+
+void swap(int *a, int *b) {
+    // Implementa swap aquí, recuerda no usar printf o similares.
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 int main(void){
-    printf("Hola mundo");
+    int a = 25;
+    int b = 13;
+    swap(&a, &b);
+    printf("%d %d", a, b);
 }
