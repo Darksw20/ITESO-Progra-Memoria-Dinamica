cf5424474658e79bcad154d564a40631306d1151
RicardoNaVi <ricardo.navarro@talent-network.org>
2020-09-14 12:13:14 -0500

    StrEqu Finished

--- EvalAsincronica1/StrEqu/StrEqu.c
+++ EvalAsincronica1/StrEqu/StrEqu.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 /**
+ * Codigo revisa que dos cadenas sean iguales
+ * 
+ * 1 si son identicas
+ * 0 si no lo son
  * 
  */
+
+int strequ(char* a, char*b);
+
+
 int main(void){
-    printf("Hola mundo");
+    char a[] = "Hola";
+    char b[] = "Hol";
+    int val = strequ(a,b);
+    printf("%d", val);
+}
+
+int strequ(char* a, char*b) {
+    int res = 1;
+    while(*a != '\0' || *b != '\0'){
+        if(*a != *b){
+            res = 0;
+        }
+        b++;
+        a++;
+    }
+    return res;
 }
