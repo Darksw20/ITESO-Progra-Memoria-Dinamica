2e4dab24cc000b7311844bf47750a42f72d9154e
Darksw20 <ricardog.navi20@gmail.com>
2020-09-23 00:42:24 -0500

    First code deck

---
+++ Deck/deck.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+struct Card{
+    void* next;
+    char type;
+};
+char* deck[] = {'10','J','Q','K','A'};
+void 
+void printDeck();
+/**
+ * Random data
+ * [10,J,Q,K,A]
+ * 
+ * 
+ * CLI =>
+ * [p]ull
+ * [e]xit
+ * if pull
+ * [t]op
+ * [b]ottom
+ * [d]iscard
+ * [e]xit
+ * 
+ * Voy a hacer un arreglo a estructuras
+ * 1=> deck
+ * y dejar un temp
+ * 
+ * estos arreglos en realidad son listas enlazadas simples
+ * 
+ */
+int main(void){
+
+}
+
+void printDeck(){
+
+}
